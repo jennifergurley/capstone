d346331eaf92529a1c51a306dd997c91e667ee83
jennifergurley <jennifergurley@gmail.com>
2024-08-25 20:12:35 -0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import statistics
-#import matplotlib
+import matplotlib
 #from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
