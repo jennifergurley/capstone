f362b631c3eee037eb4f4d788cf09e390e3f3afd
jennifergurley <jennifergurley@gmail.com>
2024-09-18 20:44:07 -0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -12,19 +12,26 @@ from sklearn.metrics import classification_report,confusion_matrix
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 
-# titles and info
+
+# create application title and overview
 st.title('RN Tenure Prediction Application')
 
 st.info('This application will allow you to predict whether an RN will be a high (10+ years), medium (6+ years), or low (< 6 years) tenure employee.')
+
 
 # load dataset
 url = 'https://raw.githubusercontent.com/jennifergurley/capstone/main/rndatafinal.csv'
 data = pd.read_csv(url)
 
-#display data
+
+# streamlit expanders are used to keep the application from being too cluttered
+# each expander will have a descriptive title for ease of selection
+
+# display dataset
 with st.expander('View Dataset'):
      st.write('RN Data Final')
      data
+
 
 # calculate and view statistics
 with st.expander('Review Data Statistics'):
@@ -38,26 +45,24 @@ with st.expander('Review Data Statistics'):
      st.write('Calculate Median Tenure')
      statistics.median(data.tenure)
 
+
 # add field to show user defined tenure categories
-
 with st.expander('Add Tenure Categories'):
-     data['tenurecategory'] = ['high tenure' if x > 10 else 'medium tenure' if x > 6 else 'low tenure' for x in data.tenure]
+     st.write('High tenure is 7+ years; medium tenure is between 3 and 7 years; low tenure is less than 3 years')
+     data['tenurecategory'] = ['high tenure' if x > 7 else 'medium tenure' if x > 3 else 'low tenure' for x in data.tenure]
 
-# STILL NEED WORK!!!
-with st.expander('Data Visualization'):
+
+# visualize datapoints versus tenure category
+with st.expander('Data Visualization for Selected Attributes'):
      st.bar_chart(data=data, x="gradrn", y="tenurecategory")
-     st.bar_chart(data=data, x="active", y="tenure")
-     st.bar_chart(data=data, x="degree", y="tenure")
-     st.bar_chart(data=data, x="volcerts", y="tenure")
-     st.bar_chart(data=data, x="rnyears", y="tenure")
-     st.bar_chart(data=data, x="lasthire", y="tenure")
-     st.bar_chart(data=data, x="avghours", y="tenure")
-     st.bar_chart(data=data, x="avgoncall", y="tenure")
-     st.bar_chart(data=data, x="lateorcallin", y="tenure")
-     st.bar_chart(data=data, x="avgshiftwork", y="tenure")
-     st.bar_chart(data=data, x="retentionrisk", y="tenure")
-     st.bar_chart(data=data, x="hiresource", y="tenure")
-     st.bar_chart(data=data, x="performance", y="tenure")
+     st.bar_chart(data=data, x="degree", y="tenurecategory")
+     st.bar_chart(data=data, x="volcerts", y="tenurecategory")
+     st.bar_chart(data=data, x="rnyears", y="tenurecategory")
+     st.bar_chart(data=data, x="lasthire", y="tenurecategory")
+     st.bar_chart(data=data, x="avghours", y="tenurecategory")
+     st.bar_chart(data=data, x="hiresource", y="tenurecategory")
+     st.bar_chart(data=data, x="performance", y="tenurecategory")
+
 
 # split into training and testing
 X = pd.DataFrame(data[['gradrn', 'degree', 'volcerts', 'rnyears', 'lasthire', 'avghours', 'avgoncall', 'lateorcallin', 'avgshiftwork', 'retentionrisk', 'hiresource', 'performance']])
@@ -80,10 +85,18 @@ with st.expander('Training and Test Sets'):
      st.write('y_test')
      y_test
 
-# 'retentionrisk', 'hiresource', 'performance'
-# user inputs
+
+# create sidebar to get user inputs
 with st.sidebar:
-     st.header('User Input')
+     
+# user input to set KNN parameters
+     st.header('KNN parameters')
+     udneighbors = st.slider ('Select KNN neighbors: ', 2, 30, 10)
+     udmetric = st.selectbox ('Select KNN metric: ', ('minkowsky', 'euclidean'))
+
+# user input to predict high tenure
+     st.header('Try it!')
+     st.write('Predict high tenure based on user selected parameters')
      gradrn = st.selectbox ('Attended Grad RN Program?:', ('No', 'Yes'))
      degree = st.selectbox ('Highest RN Degree?:', ('Associates', 'Bachelors', 'Masters'))
      volcerts = st.slider ('Number of non-Required Certifications:', 0, 4, 0)
@@ -95,7 +108,8 @@ with st.sidebar:
      retentionrisk = st.selectbox ('What is the employees current retention risk rating?', ('B', 'A', 'C'))
      hiresource = st.selectbox ('What is the employees hire source?', ('Job Board', 'Website', 'Referral', 'Newspaper'))
      performance = st.selectbox ('What is the employees latest performance score?', ('Average', 'Below Average', 'Above Average'))
-     
+
+
 # create input feature dataset
 inputdata = {'gradrn': gradrn,
              'degree': degree,
@@ -127,9 +141,10 @@ inputdata = {'gradrn': gradrn,
 with st.expander('User Input Data'):
      inputdata
      
+
 # build and fit model
 
-knn = KNeighborsClassifier(n_neighbors=10, metric='minkowski')
+knn = KNeighborsClassifier(n_neighbors=udneighbors, metric=udmetric)
 
 knn.fit(X_train_hot,y_train.values.ravel())
 
